369baa4f8cd6e7ee8834304b178138fa6269136d
Zachary Ratliff <ratliffzachary@yahoo.com>
2018-01-12 17:00:09 -0600

    Add files via upload

---
+++ NumUtility.hs
@@ -0,0 +1,3 @@
+module NumUtility where
+
+percent n d = ((fromIntegral n) / (fromIntegral d)) * 100
---
+++ combination.hs
@@ -0,0 +1,41 @@
+module Combination where
+
+import Data.List
+import NumUtility 
+
+--Regular factorial definition
+factorial::Int->Int
+factorial n 
+	| n < 1 = 1
+	| otherwise = n * factorial (n - 1)
+
+--Alternative factorial definition for reducing combinatorial calculations
+factorial'::Int->Int->Int
+factorial' n k 
+	| n == k = 1
+	| otherwise = n * factorial' (n-1) k
+
+
+nchoosek::Int->Int->Int
+nchoosek n k = a `div` b where a = factorial' n (n-k)
+			       b = factorial k
+
+getTwayCombinationCount::[a]->Int->Int
+getTwayCombinationCount xs t = nchoosek (length xs) t
+
+getTwaySequenceCount::[a]->Int->Int
+getTwaySequenceCount xs t = (getTwayCombinationCount xs t) * factorial t 
+
+--getTwayCombinations::[a]->Int->[[a]]
+--getTwayCombinations xs t = filter (\ys -> length ys == t) (subsequences xs)
+
+--getTwaySequences xs t = concat (map (permutations) (getTwayCombinations xs t))
+
+findTwaySequences t xs = filter (\ys -> length ys == t) (subsequences xs) 
+
+countTwaySequences t xs = length (findTwaySequences t xs)
+
+findAllTwaySequences t (x:[]) = findTwaySequences t x
+findAllTwaySequences t (x:xs) = nub (findTwaySequences t x ++ findAllTwaySequences t xs)
+
+measureTwayCoverage t xs ys = percent (length (findAllTwaySequences t xs)) (getTwaySequenceCount ys t)
